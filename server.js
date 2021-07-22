56622eb04220257a90aa09bfbcf405fc2e4eb7b7
hdachepalli <hdachepalli@deakin.edu.au>
2021-07-22 21:58:06 +1000

    updated - added global nodemon and changed npm to start with nodemon - added get api

--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 var express = require("express");
 var app = express()
+
+
+app.use(express.static(__dirname+'/public'))
+
 var port = process.env.port || 3000;
+
+const subNumbers = (a,b) =>{
+    var a = parseInt(a)
+    var b = parseInt(b)
+    var result = a-b;
+    return result;
+    
+}
+
+app.get("/subTwoNumbers",(req,res) =>{
+    var a = req.query.a;
+    var b = req.query.b;
+    var result = subNumbers(a,b)
+    res.json({statuscode: 200, data: result, message:"Success"})
+})
+
 
 app.listen(port,()=>{
     console.log("The Application has started on port:",port)
